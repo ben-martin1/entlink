6d773d7029d6089464308f142ceae63a62217a5c
benmartin <ben.mclane.martin@gmail.com>
2024-10-23 14:05:21 -0700

    change to datetime

--- enterlinked/src/components/CreateSchedule.jsx
+++ enterlinked/src/components/CreateSchedule.jsx
@@ -7,13 +7,13 @@ const ScheduleCreator = () => {
     const [deletedEvents, setDeletedEvents] = useState([]);
 
     const changeEvent = (e, i) => {
-        for (const ev in eventList) {
-            if(ev.index === i){
-                setEvents();
-                ev.date = e.target.value;
-                break;
+        const new_date = e.target.value;
+        setEvents(old_evs => 
+            old_evs.map((ev) => ev.index === i? e.target.value:ev)
+        )
+            for (const ev in eventList){
+                console.log(ev.date);
             }
-        }
     };
 
     const deleteEvent = (e) => {
@@ -25,14 +25,15 @@ const ScheduleCreator = () => {
         setEvents(eventList => [...eventList, {index:eventList.length, date:new_date }]);
         console.log(eventList);
     };
+
     return(
         <div className="w-full">
             {eventList.length === 0 ? <div onClick={addEvent} className="bg-green-500 rounded-lg p-3 hover:cursor-pointer">+</div>:<></>}
             {eventList.map(event => (
-                <div key={event.index} className="w-full flex items-center justify-evenly">
+                <div key={event.index} className="w-full py-3 flex items-center justify-evenly">
                     <div onClick={deleteEvent} className="bg-red-500 rounded-lg p-3 hover:cursor-pointer">x</div>
                     <form>
-                        <input type="date" onChange={(e) => changeEvent(e, event.index)} className="w-full bg-black rounded-lg p-4 text-white"></input>
+                        <input type="datetime-local" onChange={(e) => changeEvent(e, event.index)} className="w-full bg-black rounded-lg p-4 text-white"></input>
                     </form>
                     <div onClick={addEvent} className="bg-green-500 rounded-lg p-3 hover:cursor-pointer">+</div>
                 </div>
