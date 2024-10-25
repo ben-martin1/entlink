bf63c341e2b481e266fccc9da12a7dc0c976d057
benmartin <ben.mclane.martin@gmail.com>
2024-10-24 18:49:36 -0700

    fixed non-unique key bug

--- enterlinked/src/components/CreateSchedule.jsx
+++ enterlinked/src/components/CreateSchedule.jsx
@@ -5,18 +5,30 @@ const ScheduleCreator = () => {
     // initialize two blank-array states for live events and deleted events
     const [eventList, setEvents] = useState([ {index:0, date: new Date(), duration:4} ]);
     const [deletedEvents, setDeletedEvents] = useState([]);
+    const [eventIndex, setEventIndex] = useState(0);
 
     const default_date = new Date().toISOString().slice(0,16);
 
     const changeEvent = (e, i) => {
+        debugger;
         const value = e.target.value;
-        let new_date = new Date(value);
-        let new_duration = eventList[i].duration;
+        let new_duration;
+        let new_date;
+
+        // get both date and duration of event with passed index
+        eventList.map((ev) => {
+            if (ev.index === i){
+                new_date = ev.date;
+                new_duration = ev.duration;
+            } 
+        });
 
         if (e.target.name === "duration"){
-            new_date = eventList[i].date;
             new_duration = Number(value);
+        } else{
+            new_date = new Date(value);
         }
+
         setEvents(old_evs => 
             old_evs.map((ev) => ev.index === i? {...ev, date:new_date, duration:new_duration }  : ev)
         );
@@ -34,10 +46,11 @@ const ScheduleCreator = () => {
 
     const addEvent = () => {
         const new_date = new Date();
-        setEvents(eventList => [...eventList, {index:eventList.length, date:new_date, duration:4 }]);
+        setEvents(eventList => [...eventList, {index:eventIndex+1, date:new_date, duration:4 }]);
+        setEventIndex(prevIndex => prevIndex+1);
         console.log(eventList);
     };
-
+    
     return(
         <div className="w-full flex flex-col items-center">
             {eventList.map(event => (
@@ -50,6 +63,10 @@ const ScheduleCreator = () => {
                 </div>
             ))}
             <div onClick={addEvent} className="w-16 text-center bg-green-500 rounded-lg p-3 hover:cursor-pointer">+</div>
+            <h1>Schedule</h1>
+            {eventList.map(event => (
+                <p key={event.index}>{event.date.toLocaleDateString()} {event.duration} hours</p>
+            ))}
         </div>
     );
 };
