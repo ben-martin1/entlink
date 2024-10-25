8beb541608fe4c13d75610ee84f51e7ddb629b3e
benmartin <ben.mclane.martin@gmail.com>
2024-10-24 18:02:21 -0700

    fixed event change, added event deletion

--- enterlinked/src/components/CreateSchedule.jsx
+++ enterlinked/src/components/CreateSchedule.jsx
@@ -3,41 +3,53 @@ import DateLine from "./DateLine";
 
 const ScheduleCreator = () => {
     // initialize two blank-array states for live events and deleted events
-    const [eventList, setEvents] = useState([]);
+    const [eventList, setEvents] = useState([ {index:0, date: new Date(), duration:4} ]);
     const [deletedEvents, setDeletedEvents] = useState([]);
 
+    const default_date = new Date().toISOString().slice(0,16);
+
     const changeEvent = (e, i) => {
-        const new_date = e.target.value;
+        const value = e.target.value;
+        let new_date = new Date(value);
+        let new_duration = eventList[i].duration;
+
+        if (e.target.name === "duration"){
+            new_date = eventList[i].date;
+            new_duration = Number(value);
+        }
         setEvents(old_evs => 
-            old_evs.map((ev) => ev.index === i? e.target.value:ev)
-        )
-            for (const ev in eventList){
-                console.log(ev.date);
-            }
+            old_evs.map((ev) => ev.index === i? {...ev, date:new_date, duration:new_duration }  : ev)
+        );
     };
 
-    const deleteEvent = (e) => {
-        console.log("DELETED");
+    const deleteEvent = (i) => {
+        // add event to deleted events
+        const old_ev = eventList[i];
+        setDeletedEvents(deletedEventList => [...deletedEventList, old_ev]);
+        console.log(deletedEvents);
+
+        // delete event
+        setEvents(old_evs => old_evs.filter((ev) => ev.index !==i));
     };
 
     const addEvent = () => {
         const new_date = new Date();
-        setEvents(eventList => [...eventList, {index:eventList.length, date:new_date }]);
+        setEvents(eventList => [...eventList, {index:eventList.length, date:new_date, duration:4 }]);
         console.log(eventList);
     };
 
     return(
-        <div className="w-full">
-            {eventList.length === 0 ? <div onClick={addEvent} className="bg-green-500 rounded-lg p-3 hover:cursor-pointer">+</div>:<></>}
+        <div className="w-full flex flex-col items-center">
             {eventList.map(event => (
                 <div key={event.index} className="w-full py-3 flex items-center justify-evenly">
-                    <div onClick={deleteEvent} className="bg-red-500 rounded-lg p-3 hover:cursor-pointer">x</div>
+                    <div onClick={() => deleteEvent(event.index)} className="bg-red-500 rounded-lg p-3 hover:cursor-pointer">x</div>
                     <form>
-                        <input type="datetime-local" onChange={(e) => changeEvent(e, event.index)} className="w-full bg-black rounded-lg p-4 text-white"></input>
+                        <input type="datetime-local" defaultValue={default_date} onInput={(e) => changeEvent(e, event.index)} className="bg-black rounded-lg p-4 text-white"></input>
+                        <input type="number" defaultValue={4} min={0} name="duration" onInput={(e) => changeEvent(e, event.index)} className="bg-black rounded-lg p-4 text-white"></input>
                     </form>
-                    <div onClick={addEvent} className="bg-green-500 rounded-lg p-3 hover:cursor-pointer">+</div>
                 </div>
             ))}
+            <div onClick={addEvent} className="w-16 text-center bg-green-500 rounded-lg p-3 hover:cursor-pointer">+</div>
         </div>
     );
 };
