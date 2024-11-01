6b3b0e056af8027bdebcb6a9a9346ec4008c2ef3
benmartin <ben.mclane.martin@gmail.com>
2024-11-01 11:02:01 -0700

    figured out date conflict logic

--- enterlinked/src/utilities/conversionService.js
+++ enterlinked/src/utilities/conversionService.js
@@ -44,58 +44,76 @@ const getParsedSchedule = (event) => {
 
 // TODO - consider sorting the dates for faster comparison? should i sort them here or upon creation?
 const getAvailableTimes = (open, unavail) => {
+    debugger;
+    if (!unavail){
+        return open;
+    }
 
     const goodDates = [];
     open.forEach(open_date => {
         unavail.forEach(unavail_date => {
             console.log(`Checking ${open_date.start_date} against ${unavail_date.start_date}`);
-
+            
             // start same day - check if there is a time conflict
             if (open_date.start_date.toDateString() === unavail_date.start_date.toDateString()){
                 const open_date_start = open_date.start_date.getTime(); 
                 const open_date_end = open_date.end_date.getTime(); 
                 const unavail_date_start = unavail_date.start_date.getTime(); 
                 const unavail_date_end = unavail_date.end_date.getTime(); 
-
-                if (unavail_date_start > open_date_end){ // case: conflict starts after open_date ends - continue
-                    console.log("Good date wide - conflict ends early");
-                    goodDates.push(open_date);
+                
+                // complete conflict case, no part of open date is avail
+                if (unavail_date_start <= open_date_start && unavail_date_end >= open_date_end){
+                    return; 
                 }
-                if (unavail_date_end < open_date_start){ // case: conflict ends before open_date starts - continue
+                
+                // nonconflict case 1: conflict ends before open_date starts - continue
+                if (unavail_date_end < open_date_start){ 
                     console.log("Good date wide - good date ends late");
                     goodDates.push(open_date);
+                    return;
                 }
-                if (unavail_date_end >= open_date_start && unavail_date_start <= open_date_start){ // case: conflict starts before open_date and ends after open_date starts
-                    if (unavail_date_end < open_date_end){ // some of open_date is still avail
-                        // TODO - create a new date equal to open date except make start time === unavail_date_end and return it. from createschedule, update the state
-                        console.log("Good date cut short at beginning");
-                        const new_good_date = {start_date:unavail_date.end_date, end_date:open_date.end_date};
-                        goodDates.push(new_good_date);
-                    }
+
+                // nonconflict case 2: conflict starts after open_date ends - continue
+                if (unavail_date_start > open_date_end){ 
+                    console.log("Good date wide - conflict ends early");
+                    goodDates.push(open_date);
+                    return;
                 }
-                if (unavail_date_start >= open_date_end){ // case: event starts during avail event
-                    if (unavail_date_end < open_date_end){ // case: will need to split the event into 2 - for each available slot
-                        const first_good_date = {start_date:open_date.start_date, end_date:unavail_date.end_date};
+
+                // partial conflict case 1: conflict starts before open and ends after open starts
+                if (unavail_date_start <= open_date_start && unavail_date_end >= open_date_start){ 
+                    console.log("Good date cut short at beginning");
+                    const new_good_date = {start_date:unavail_date.end_date, end_date:open_date.end_date};
+                    goodDates.push(new_good_date);
+                }
+
+                if (unavail_date_start > open_date_start){
+
+                    // partial conflict case 2: open split in 2 events - before conflict starts and after it ends
+                    if (unavail_date_end < open_date_end){ 
+                        const first_good_date = {start_date:open_date.start_date, end_date:unavail_date.start_date};
                         const second_good_date = {start_date:unavail_date.end_date, end_date:open_date.end_date};
                         console.log("Good date split");
                         goodDates.push(first_good_date);
                         goodDates.push(second_good_date);
                     }
-                    else {
-                        // TODO create new date equal to open date execpt END time === unavail_date_START. return it then update state in createSchedule
+
+                    // partial conflict case 3: open cut at end
+                    else { 
                         const new_good_date = {start_date:open_date.start_date, end_date:unavail_date.start_date};
                         goodDates.push(new_good_date);
                     }
                 }
             }
             else{
+                // nonconflict case 3: different dates entirely
                 goodDates.push(open_date);
             }
         });
     });
     console.log("goodDates: ");
     console.log(goodDates);
-    return open;
+    return goodDates;
 };
 
 const conversionService = {getParsedSchedule, getAvailableTimes, changeEvent}
